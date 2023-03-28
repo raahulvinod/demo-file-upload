8488d400541d97b6690cdc6d1f19b35cca0c6809
Rahul vinod <rahulvinod135@gmail.com>
2023-03-28 16:00:07 +0530

    Added file upload

---
+++ public/scripts/file-preview.js
@@ -0,0 +1,20 @@
+const filePickerElement =  document.getElementById('image');
+const imagePreviewElement = document.getElementById('image-preview');
+
+
+function showPreview(event) {
+   const files = filePickerElement.files;
+   if (!files || files.length === 0) {
+    imagePreviewElement.style.display = 'none';
+     return;
+   }
+
+
+   const pickedFile = files[0];
+   imagePreviewElement.src = URL.createObjectURL(pickedFile);
+   imagePreviewElement.style.display = 'block';
+}
+
+filePickerElement.addEventListener('change', showPreview );
+
+
